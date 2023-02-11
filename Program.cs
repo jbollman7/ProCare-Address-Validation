a55d76e3ea3ca2725ae255afb027df9c1f3b3ff2
Joe Bollman <joseph.bollman@mortgagecadence.com>
2023-02-10 22:48:57 -0700

    If call takes longer than n milliseconds, cancellation token will cancel the request. -- Working

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
 namespace Procare.AddressValidation.Tester
 {
     using System;
+    using System.Threading;
     using System.Threading.Tasks;
 
     internal sealed class Program
@@ -20,9 +21,17 @@ namespace Procare.AddressValidation.Tester
             // var request = new AddressValidationRequest { Line1 = "1 W Main", City = "Medford", StateCode = "OR", ZipCodeLeading5 = "97501" };
             // var request = new AddressValidationRequest();
             var request = new AddressValidationRequest { Line1 = "1125 17th St Ste 1800", City = "Denver", StateCode = "CO", ZipCodeLeading5 = "80202" };
-
-            var response = await addressService.GetAddressesAsync(request).ConfigureAwait(false);
-            Console.WriteLine(response);
+            using var cts = new CancellationTokenSource();
+            cts.CancelAfter(TimeSpan.FromMilliseconds(700));
+            try
+            {
+                var response = await addressService.GetAddressesAsync(request, cts.Token).ConfigureAwait(false);
+                Console.WriteLine(response);
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("The operation was cancelled");
+            }
         }
     }
 }
